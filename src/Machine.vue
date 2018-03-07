49c82626473ff0c87b5d459e7e7e81ae3b4fbadd
Ana-Maria Olmo <ana-maria.olmo@campus-numerique-in-the-alps.com>
2018-03-07 18:07:17 +0100

    listes machines props

--- src/Machine.vue
+++ src/Machine.vue
@@ -1,29 +1,67 @@
 <template>
     <div>
-        <h1>Machine {{Machine.name}}</h1>
-       <!-- <h3>Status {{Machine.status}}</h3>-->
-        <h3 v-if ="Machine.status":style="{'color':'green'}">Status OK</h3>
+
+        <h1>Machine {{name}}</h1>
+        <h3 v-if ="status":style="{'color':'green'}">Status OK</h3>
         <h3 v-else :style="{'color':'red'}">Status KO</h3>
-        <h5>Last time checked : {{Machine.checkedAt}}</h5>
+        <h5>Last time checked : {{checkedAt}}</h5>
+        <!-- nouveau
+        <h1>Machine{{name}}</h1>
+        <h3 v-if ="status" class="green">{{MessageOk}}</h3>
+        <h3 v-else  class="red">{{MessageKo}}</h3>
+        <h5>Last time checked : {{date(checkedAt)}}</h5>-->
+
     </div>
 </template>
 
-
 <script>
     export default {
-        data() {
-            return {
-                Machine: {
-                    name: 'What else ?',
-                    status: false,
-                    checkedAt: new Date(),
+        name: 'Machine',
+        props: ['name', 'status', 'checkedAt']
+        // data() {
+        //     return {
+        //         Machine: {
+        //             name: 'What else ?',
+        //             status: true,
+        //             checkedAt: new Date(),
 
-                }
-            }
-        }
+               // }
+            //}
+        //}
     }
+
 </script>
 
-<style>
+<!--<script>-->
 
+    <!--export default {-->
+        <!--name: 'Machine',-->
+        <!--props: ['name', 'status', 'checkedAt'],-->
+        <!--data() {-->
+            <!--return {-->
+                <!--MessageOk: 'status OK',-->
+                <!--MessageKo: 'Status KO'-->
+            <!--}-->
+        <!--},-->
+            <!--methods: {-->
+                <!--dateFormat : function(date){-->
+                    <!--return date.toLocaleString();-->
+                <!--}-->
+            <!--}-->
+        <!--};-->
+
+<!--</script>-->
+
+<!--<style>
+</style>-->
+
+<style>
+    .red {
+        color:red;
+    }
+    .green {
+        color: green;
+    }
 </style>
+
+
--- src/MachinesList.vue
+++ src/MachinesList.vue
@@ -1,17 +1,41 @@
 <template>
    <div>
-    <h1>{{ msg}}</h1>
+    <h1>Liste des machines</h1>
+     <Machine  v-for="machine in machines"
+               :key="machine.id"
+               v-bind:name="machine.name"
+               v-bind:status="machine.status"
+               v-bind:checkedAt="machine.checkedAt.toLocaleString()">
+
+
+     </Machine>
+
    </div>
 </template>
 
 
 <script>
+    import Machine from './Machine.vue';
+
     export default {
-        name: 'List',
+        components:{Machine},
+        name:'List',
         data() {
-            return {
-                msg: 'Machines list'
-            }
+                return {
+                    machines: [
+                        {
+                            id:1,
+                            name: 'What else ?',
+                            status: true,
+                            checkedAt: new Date(),
+                        },
+                        {
+                            id:2,
+                            name: 'Really?',
+                            status: false,
+                            checkedAt: new Date(),
+                        }]
+                }
         }
     };
 
