8035a7002e4b28bdd43f2976ad4ba6f25bef8bc6
Ana-Maria Olmo <ana-maria.olmo@campus-numerique-in-the-alps.com>
2018-03-08 11:19:05 +0100

    creation map

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <h1>{{ msg}}</h1>
-        <!--<gmap-map-->
-                <!--:center="{lat:10, lng:10}"-->
-                <!--:zoom="7"-->
-                <!--:class="google-map"-->
-        <!--&gt;-->
-        <!--</gmap-map>-->
+        <gmap-map
+                :center="{lat:48, lng:2}"
+                :zoom="7"
+                class="google-map"
+        >
+        </gmap-map>
     </div>
 </template>
 
@@ -29,6 +29,7 @@
     .google-map {
         width: 800px;
         height: 600px;
+      margin: auto;
 
     }
 </style>
